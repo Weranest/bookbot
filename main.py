5a467eee91b3609b07fb460648794e772725b0e9
Weranest <weranest@gmail.com>
2024-05-30 21:48:39 +0300

    Add: word count and letter count functions

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print("hello world")
+def word_count(text):
+   words = text.split()
+   return len(words)
+
+
+def letter_count(text):
+    letters = {}
+    lower_text = text.lower()
+    for letter in lower_text:
+        if letter in letters.keys():
+            letters[letter] += 1
+        else:
+            letters[letter] = 1
+    return letters
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    print(word_count(file_contents))
+    print(letter_count(file_contents))
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
