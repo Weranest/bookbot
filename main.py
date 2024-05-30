853b2215560e6a665a87e8389347a4f9b4a6d326
Weranest <weranest@gmail.com>
2024-05-30 22:34:31 +0300

    Add: added sorting function

--- main.py
+++ main.py
@@ -17,11 +17,27 @@ def letter_count(text):
             letters[letter] = 1
     return letters
 
+def dict_to_list(dict):
+    dict_list = []
+    for key in dict:
+        temp_dict = {"character":"", "number":0}
+        temp_dict["character"] = key
+        temp_dict["number"] = dict[key]
+        print(temp_dict)
+        dict_list.append(temp_dict)
+    print(dict_list)
+    return dict_list
+
+def sort_crit(dict_list):
+    return dict_list["number"]
+
 def main():
     file_path = "books/frankenstein.txt"
     text = get_text(file_path)
+    letters = letter_count(text)
+    prepped_letters = dict_to_list(letters)
     print (f"Wordcount for the selected book: {word_count(text)} words")
-    print(f"Letter distribution for selected book: {letter_count(text)}")
+    print(f"Letter distribution for selected book: {prepped_letters.sort(reverse=True, key=sort_crit)}")
 
 main()
 
